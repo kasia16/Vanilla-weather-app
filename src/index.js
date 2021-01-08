d4c771f53d3d192d30e09061120cd5baff97e07d
kasia16 <76659769+kasia16@users.noreply.github.com>
2021-01-08 18:03:55 +0100

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+return ` ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
 
 console.log(response.data);
 
-let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
+let dateElement = document.querySelector("#date");
+
+let temperatureElement = document.querySelector("#temperature");
 let conditionsElement = document.querySelector("#conditions");
 let windElement = document.querySelector("#wind");
 let feelElement = document.querySelector("#feel");
@@ -12,7 +28,8 @@ cityElement.innerHTML = response.data.name;
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 conditionsElement.innerHTML = response.data.weather[0].main;
 windElement.innerHTML = Math.round(response.data.wind.speed);
-feelElement.innerHTML =Math.round(response.data.main.feels_like);
+feelElement.innerHTML = Math.round(response.data.main.feels_like);
+dateElement.innerHTML = formatDate(response.data.dt *1000);
 }
 
 
