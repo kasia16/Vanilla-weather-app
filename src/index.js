9769702eb2e61fb3e5f2413f825ba9f388e6e927
kasia16 <76659769+kasia16@users.noreply.github.com>
2021-01-09 00:38:18 +0100

    fixed URL

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ cityElement.innerHTML = response.data.name;
 
 iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 iconElement.setAttribute("alt", response.data.weather[0].description);
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
