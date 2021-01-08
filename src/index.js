a38ac3fc2f239bfddf4bbe425d0557dbd95e31eb
kasia16 <76659769+kasia16@users.noreply.github.com>
2021-01-08 18:59:13 +0100

    Added icon

--- src/index.js
+++ src/index.js
@@ -14,25 +14,37 @@ return ` ${hours}:${minutes}`;
 
 function displayTemperature(response){
 
+
 let cityElement = document.querySelector("#city");
 let dateElement = document.querySelector("#date");
 
+let iconElement = document.querySelector("#icon");
 let temperatureElement = document.querySelector("#temperature");
 let conditionsElement = document.querySelector("#conditions");
 let windElement = document.querySelector("#wind");
 let feelElement = document.querySelector("#feel");
 
+
+
 cityElement.innerHTML = response.data.name;
+
+iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+iconElement.setAttribute("alt", response.data.weather[0].description);
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 conditionsElement.innerHTML = response.data.weather[0].main;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 feelElement.innerHTML = Math.round(response.data.main.feels_like);
 dateElement.innerHTML = formatDate(response.data.dt *1000);
+
 }
 
 
 let apiKey = "2a93853098f7d48795c997915462e083";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 axios.get(apiUrl).then(displayTemperature);
